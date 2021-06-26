b3d51c4ee1c804a3e858192da590c5efd36793fa
Rafaela França <rafaelafrancamedeiros@gmail.com>
2021-06-25 22:26:59 -0300

    ajuste consumo api

--- src/pages/Heros/index.js
+++ src/pages/Heros/index.js
@@ -1,45 +1,45 @@
 import React, {useState, useEffect} from "react";
 import Menu from "../../components/Menu";
 import Footer from "../../components/Footer";
-import dados from "../../service/dados";
+//import dados from "../../service/dados";
 //import Axios from 'axios';
 import './style.css';
 
 const Heros = ()=>{
-    //const [personagem, setPersonagem] = useState([])
+    const [personagens, setPersonagem] = useState([])
     const [filtroPersonagem, setFiltroPersonagem] = useState([])
     const [busca, setBusca] = useState('')
 
 //https://superheroapi.com/api/4055034404614169/character-246/powerstats
 //`https://superheroapi.com/api/4055034404614169/character-${id}`
-
-    //useEffect(()=>{
-        //onst pegaDados = async ()=>{
-            //const resposta = await Axios.get('https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json')
-            //console.log(resposta)
-            //const dados = await resposta.dados
-            //setPersonagem(dados)
-            //console.log(dados)
-        //}
-        //pegaDados()
-    //},[])
+//https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json
+    useEffect(()=>{
+        fetch('https://rickandmortyapi.com/api/character')
+            .then(resposta => resposta.json())
+            .then(dados => setPersonagem(dados.results))
+        },[])  
 
     useEffect(()=>{
         setFiltroPersonagem(
-            dados.filter(dados =>{
-                return dados.nome.includes(busca)
+            personagens.filter(personagem =>{
+                return personagem.name.toLowerCase().includes(busca)
             })
         )
-    }, [busca])
+    }, [busca, personagens])
 
     return(
         <>
             <Menu/>
             <input type="text" placeholder="Nome do personagem" onChange={e=>{setBusca(e.target.value)}}/>
-            {filtroPersonagem.map(dados=>(
-                <div key={dados.id}>
-                    <p>{dados.name}</p>
-                    <img src={dados.image} alt={dados.name}/>
+            {filtroPersonagem.map(personagem=>(
+                <div key={personagem.id}>
+                    <div className="card">
+                        <div className="discricao">
+                            <p>{personagem.name}</p>
+                            <p>{personagem.species}</p>
+                        </div>
+                        <img className="img" src={personagem.image} alt={personagem.name}/>
+                    </div>
                 </div>
             ))}
             <Footer/>
